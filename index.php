1b94c0c746182e00909b5eb0f6895eac849ad994
Matheus <mdmaia91@gmail.com>
2022-08-26 17:50:29 -0300

    homepage template

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda ANS</title>
+</head>
+<body>
+    <header>
+        <h1>Agenda ANS</h1>
+        <nav>
+        <a href = "index.php?menuop=home">Home</a>
+        <a href = "index.php?menuop=contatos">Contatos</a>
+        <a href = "index.php?menuop=tarefas">Tarefas</a>
+        <a href = "index.php?menuop=eventos">Eventos</a>
+    <header>
+        <main>
+            <?php 
+                $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+                    switch ($menuop) {
+                        case 'home':
+                            include("paginas/home/home.php");
+                            break;
+                        case 'contatos':
+                            include("paginas/contatos/contatos.php");
+                            break;
+                        case 'tarefas':
+                            include("paginas/tarefas/tarefasphp");
+                            break;
+                        case 'eventos':
+                            include("paginas/eventos/eventos.php"); 
+                            break;
+                        default:
+                            break;
+                    }
+            ?>
+        </main>
+</body>
+</html>
